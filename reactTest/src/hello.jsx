3dd4a781e0365755c0ed8650991e4e2fe72a1877
ZachMcGuire <zach.mcguire09@gmail.com>
2024-02-07 10:21:49 -1000

    Testing

---
+++ reactTest/src/hello.jsx
@@ -0,0 +1,5 @@
+function Hello() {
+    return <h1>This is just another test.</h1>
+}
+
+export default Hello;
--- reactTest/src/main.jsx
+++ reactTest/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Greeting from './Greeting.jsx'
+import Hello from './hello.jsx'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Greeting />
+    <Hello />
   </React.StrictMode>,
 )
